f6bc526bec46b7245cdf929f440837658f416e3c
jiangxufeng <jiangxufeng1998@qq.com>
2019-01-03 22:57:28 +0800

    修复UserLogin中的bug

--- account/UserLogin.py
+++ account/UserLogin.py
@@ -1,18 +1,18 @@
 import requests
-from rewrite.exception import ServerWrong
+# from rewrite.exception import ServerWrong
 
 headers = {
     'Connection': 'keep-alive',
-    'Host': 'seat.lib.whu.edu.cn',
+    'Host': 'seat.lib.whu.edu.cn:8443',
     'Accept': 'image/webp,image/*,*/*;q=0.8',
     'Accept-Language': 'zh-CN,zh;q=0.8',
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.98 Safari/537.36 LBBROWSER',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36',
     'Accept-Encoding': 'gzip, deflate, sdch'
 }
 
 
 def Userlogin(username, password):
-    url3 = 'http://seat.lib.whu.edu.cn/rest/auth?username=%s&password=%s' % (username, password)
+    url3 = 'https://seat.lib.whu.edu.cn:8443/rest/auth?username=%s&password=%s' % (username, password)
     proxy = {
         'http': None,
         'https': None,
@@ -23,10 +23,12 @@ def Userlogin(username, password):
     except KeyError:
         raise ServerWrong
     if result == 'success':
-        url_2 = 'http://seat.lib.whu.edu.cn/rest/v2/user?token=%s' % (content['data']['token'])
+        url_2 = 'https://seat.lib.whu.edu.cn:8443/rest/v2/user?token=%s' % (content['data']['token'])
         json_2 = requests.get(url_2, headers=headers, proxies=proxy).json()
         return json_2['data']['name']
     else:
         return False
 
 
+if __name__ == "__main__":
+    print(Userlogin('2016301500226', '142510'))
